25997865e761e793594dd3f190aff959f0b17667
amiten_l <20240106@s.ubaguio.edu>
2026-03-16 08:44:53 +0800

    Generate supplierDetails component

---
+++ src/app/components/suppliers-details/suppliers-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suppliers-details',
+  imports: [],
+  templateUrl: './suppliers-details.component.html',
+  styleUrl: './suppliers-details.component.css'
+})
+export class SuppliersDetailsComponent {
+
+}
