32db7c799c218d1e5b163e067408483d63af7764
amiten_l <20240106@s.ubaguio.edu>
2026-03-16 09:17:10 +0800

    add supplierDetails component logic, template eith two-way binding and style

--- src/app/components/suppliers-details/suppliers-details.component.ts
+++ src/app/components/suppliers-details/suppliers-details.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Supplier } from '../../models/supplier.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SupplierService } from '../../services/supplier.service';
 
 @Component({
   selector: 'app-suppliers-details',
@@ -6,6 +9,31 @@ import { Component } from '@angular/core';
   templateUrl: './suppliers-details.component.html',
   styleUrl: './suppliers-details.component.css'
 })
-export class SuppliersDetailsComponent {
+export class SuppliersDetailsComponent implements OnInit {
+  supplier: Supplier | undefined;
+  supplierId!: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private supplierSevices: SupplierService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.supplierId = +params['id'];
+      this.supplier = this.supplierSevices.getSupplierById(this.supplierId);
+    });
+  }
+
+  saveChanges():void{
+    if (this.supplier){
+      this.supplierSevices.updateSupplier(this.supplier);
+    }
+  }
+
+  goBack(): void{
+    this.router.navigate(['/suppliers']);
+  }
 
 }
