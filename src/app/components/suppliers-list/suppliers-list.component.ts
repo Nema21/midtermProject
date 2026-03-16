6d12d0905285310863123ff957e12c58a77d8c8d
amiten_l <20240106@s.ubaguio.edu>
2026-03-16 08:56:53 +0800

    add supplierList with table display

--- src/app/components/suppliers-list/suppliers-list.component.ts
+++ src/app/components/suppliers-list/suppliers-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Supplier } from '../../models/supplier.interface';
+import { SupplierService } from '../../services/supplier.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-suppliers-list',
@@ -6,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './suppliers-list.component.html',
   styleUrl: './suppliers-list.component.css'
 })
-export class SuppliersListComponent {
+export class SuppliersListComponent implements OnInit {
+  suppliers: Supplier[] = [];
+  constructor(
+    private supplierService: SupplierService, 
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.suppliers = this.supplierService.getSuppliers();
+  }
+
+  viewSupplierDetails(id: number): void{
+    this.router.navigate(['/suppliers', id]);
+  }
 
 }
