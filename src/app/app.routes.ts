38a366a2413699430ffcdf80690d31586fb090f1
amiten_l <20240106@s.ubaguio.edu>
2026-03-16 09:05:24 +0800

    add supplierList component template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SuppliersListComponent } from './components/suppliers-list/suppliers-list.component';
+import { SuppliersDetailsComponent } from './components/suppliers-details/suppliers-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo:'/suppliers', pathMatch:'full'},
+    {path:'suppliers', component: SuppliersListComponent},
+    {path:'suppliers/:id', component: SuppliersDetailsComponent}
+];
